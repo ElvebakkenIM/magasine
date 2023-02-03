b2d72a8bb8369dc112467b088192f5c16174864e
jack lund <jacklund1@icloud.com>
2023-02-03 12:53:55 +0100

    tacking out backend

--- backend/schemas/index.js
+++
@@ -1,7 +0,0 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-import about from './about'
-
-export const schemaTypes = [about, post, author, category, blockContent]
