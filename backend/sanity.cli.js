2e99648c022aafbc35ec12c3cb5229c3337c1c50
jack lund <jacklund1@icloud.com>
2023-02-03 12:25:02 +0100

    Forandring av sanity prosjekt

---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'bbtj980d',
+    dataset: 'production'
+  }
+})
---
+++ backend/schemas/about.js
@@ -0,0 +1,15 @@
+
+
+
+export default{
+    name: 'about',
+    type: 'document',
+    title: 'About',
+    fields: [
+     {  name: 'name',
+        type: 'string',
+        title: 'Name'
+}]}
+
+
+    
