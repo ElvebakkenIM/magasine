75ba3c970e74d2e19a94d4ec41b2b3c5745e4e27
Juni E. J <jujoa007@osloskolen.no>
2023-02-03 13:28:02 +0100

    editor i index

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -2,6 +2,7 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import editor from './editor'
 import about from './about'
 
-export const schemaTypes = [about, post, author, category, blockContent]
+export const schemaTypes = [about, post, author, editor, category, blockContent]
