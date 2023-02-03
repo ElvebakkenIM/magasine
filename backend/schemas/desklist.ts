333b3f5678b07bee0abaae7e740414492c02c5ac
jack lund <jacklund1@icloud.com>
2023-02-03 12:58:49 +0100

    putting old backend

---
+++ backend/schemas/desklist.ts
@@ -0,0 +1,14 @@
+import {defineField, defineType} from 'sanity'
+
+export default {
+    name: 'navigationItem',
+    title: 'Navigation Item',
+    fields: [
+      
+    ],
+    preview: {
+      select: {
+        title: 'route.slug.current',
+      }
+    }
+  }
