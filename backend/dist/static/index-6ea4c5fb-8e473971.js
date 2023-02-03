7348b62b0664b825bbfd3e73c4280a5042885b52
jack lund <jacklund1@icloud.com>
2023-02-03 13:00:29 +0100

    removing is again

--- backend/dist/static/index-6ea4c5fb-8e473971.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPaneProvider as m,DocumentPane as a}from"./index-66ec8894-83784021.js";import"./desk-eb7b3548-822c3ec9.js";import"./index-604ed254.js";export{m as DocumentPaneProvider,a as default};
--- backend/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-import blockContent from './blockContent'
-import post from './post'
-import aboutInfo from './aboutInfo'
-import editor from './editor'
-import postType from './postType'
-
-
-export const schemaTypes = [post, aboutInfo, editor, postType, blockContent]
