819f71c07f553efe957ee1d07f83c38c106a1421
jack lund <jacklund1@icloud.com>
2023-02-13 11:10:16 +0100

    added data in display

--- svelte/src/routes/+page.js
+++ svelte/src/routes/+page.js
@@ -1,11 +1,13 @@
 import sanityClient from "@sanity/client"
 
+
 const client = sanityClient({
     projectId: "bbtj980d",
     dataset: "production",
     apiVersion: "2023-01-30",
     useCdn: false,
   });
+
 
   export async function load({ }) {
     const data = await client.fetch(`*[_type == "post"]`);
@@ -20,5 +22,7 @@ const client = sanityClient({
       body: new Error("Internal Server Error")
     };
   }
+
+  
 
  
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -5,12 +5,23 @@
 
     let author = data.about[0].author;
     console.log(author)
+
+    let body = data.about[0].body
+
+    let img = data.about[0].mainImage.asset._ref
+    console.log(img)
+
+
     
 </script>
     
     
     
 <h1>Welcome to making Magasine work {author}. Good Luck!!</h1>
+
+<p>{body}</p>
+
+<img src="{img}" alt="">
 
 
 
