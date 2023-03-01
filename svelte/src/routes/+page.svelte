99b7997353384eca573f605b3b94c833fe8ae910
jack lund <jacklund1@icloud.com>
2023-03-01 11:22:59 +0100

    adding image builder

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,27 +1,56 @@
-<script>
+<script >
 
     export let data;
-    console.log(data.about[0])
+    console.log("Data from Sanity: ", data.post)
+    console.log("Data from post: test, ", data.post[4])
 
-    let author = data.about[0].author;
-    console.log(author)
 
-    let body = data.about[0].body
+    let author = data.post[4].author;
+    // console.log(author)
 
-    let img = data.about[0].mainImage.asset._ref
-    console.log(img)
+    let body = data.post[4].body;
+
+    let img = data.post[4].mainImage.asset._ref;
+
+    let title = data.post[4].title;
+
+    //Retrives builder from sanity through imbuilder. The uses this to creat url for image. 
+    let builder = data.imgBuilder;
+
+    function urlFor(source) {
+        return builder.image(source)
+    }
+    
+  
+
 
 
     
+
+
 </script>
     
     
     
 <h1>Welcome to making Magasine work {author}. Good Luck!!</h1>
 
-<p>{body}</p>
+<h1>{title}</h1>
 
-<img src="{img}" alt="">
+
+ <div class="img"> <img src={urlFor(img).width(200).url()} >  </div>  
+ 
+ <p>{body}</p> 
+
+
+<style>
+
+.img{
+    display: flex;
+    border: 1px solid;
+    width: 200px;
+    
+}
+</style>
 
 
 
