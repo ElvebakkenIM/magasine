546acc34bf3689259870674eb427177c1edb6b1f
Jun1l1ll1 <118979783+Jun1l1ll1@users.noreply.github.com>
2023-06-02 13:49:32 +0200

    vinn og tap

--- svelte/src/routes/spill/+page.svelte
+++ svelte/src/routes/spill/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import GenererOrd, {getWord} from "./genererOrd.svelte";
     import Footer from "lib//components/footer.svelte";
     import HangmanBokstav from "lib//components/hangmanBokstav.svelte";
     
@@ -12,11 +13,11 @@
 
     let tastaturBokstaver = 'QWERTYUIOPÅASDFGHJKLØÆZXCVBNM';
 
-    let ordet = 'ELVEBAKKEN'; // Maks 11 bokstaver (endre dette i CSS)
+    let ordet = getWord(); // Maks 11 bokstaver (endre dette i CSS)
     let riktig = [];
     let feil = [];
     let skalGjette = '';
-     
+    let vunnet = false; 
 
     function bokstavKlikket(bokstav) {
         skalGjette = bokstav;
@@ -24,31 +25,64 @@
 
     function gjettDette() {
         if (skalGjette && !feil.includes(skalGjette) && !riktig.includes(skalGjette)) {
-            ordet.includes(skalGjette) ? riktig = [...riktig, skalGjette] : feil = [...feil, skalGjette]
+            ordet.includes(skalGjette) ? riktig = [...riktig, skalGjette] : feil = [...feil, skalGjette];
         }
+    }
+
+    $: riktig, harVunnet();
+    function harVunnet() {
+        let igjenIOrdet = ordet;
+        for (let bokstav of riktig) {
+            igjenIOrdet = igjenIOrdet.replace(bokstav, '');
+        }
+        if (!igjenIOrdet) {
+            vunnet = true;
+        }
+    }
+
+    function reset() {
+        riktig = [];
+        feil = [];
+        skalGjette = '';
+        vunnet = false;
     }
 </script>
 
 <div id="centerGame">
     <div id="containGame">
         <div id="bilde"><img src={feil.length == 0 ? B_0 : feil.length == 1 ? B_1 : feil.length == 2 ? B_2 : feil.length == 3 ? B_3 : feil.length == 4 ? B_4 : feil.length == 5 ? B_5 : B_6} alt="{feil.length}/6"/></div>
+        
         <div id="ikkeRiktig">
             {#each feil as bokstav}
                 <HangmanBokstav bokstav={bokstav}/>
             {/each}
         </div>
+
         <div id="ordet">
             {#each ordet as bokstav}
                 <HangmanBokstav bokstav={riktig.includes(bokstav) ? bokstav : ''} style={riktig.includes(bokstav) ? 'correct' : 'normal'}/>
             {/each}
         </div>
+
         <div id="tastatur">
+            {#if feil.length < 6 && !vunnet}
             <div id="tastaturGrid">
                 {#each tastaturBokstaver as bokstav}
                     <button on:click={() => bokstavKlikket(bokstav)} class="tastaturBokstaver"><HangmanBokstav bokstav={bokstav} style={skalGjette == bokstav ? 'selected' : feil.includes(bokstav) ? 'wrong' : riktig.includes(bokstav) ? 'correct' : 'normal'}/></button>
                 {/each}
                 <button on:click={gjettDette} id="guess">Guess</button>
             </div>
+            {:else}
+            <div class="spillSlutt">
+                {#if vunnet}
+                <h2>Godt jobbet!</h2>
+                {:else}
+                <h2 style="margin-bottom: 0px;">Bedre lykke neste gang!</h2>
+                <p style="margin-top: 0px;">Ordet var: {ordet}</p>
+                {/if}
+                <button on:click={reset}>Spill igjen</button>
+            </div>
+            {/if}
         </div>
     </div>
 </div>
@@ -127,5 +161,20 @@
 
         grid-column: 9 / 12;
         grid-row: 3;
+    }
+
+    .spillSlutt {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+    .spillSlutt button {
+        background-color: #4A9CFF;
+        color: white;
+        border: none;
+        border-radius: 1vw;
+        padding: .5vw;
+        font-size: 1.5vw;
+        cursor: pointer;
     }
 </style>
---
+++ svelte/src/routes/spill/genererOrd.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+
+    let words = [
+        'Elvebakken',
+        'Skole',
+        'MagasinE',
+        'Elevråd',
+        'Eksamen'
+    ];
+
+    export function getWord(){
+        let randomIndex = Math.floor(Math.random() * (words.length-1));
+        return words[randomIndex].toUpperCase();
+    }
+</script>
